2856d8359fd8f3fde9f85a8096cf79accd1576d2
nameofname <nameofemail@gmail.com>
2017-05-09 13:01:11 -0400

    writing tests and refining mapClosestProduct logic

--- src/mapClosestProduct.js
+++ src/mapClosestProduct.js
@@ -1,30 +1,29 @@
 "use strict";
 
+const noFractionWarning = 'WARNING! None of the values passed to mapClosestProduct are fractions. This will limit the effectiveness of your solution';
 
 module.exports = (arr1, arr2, desiredProduct) => {
 
     let closestProduct;
-    let currentSearchIdx;
+    let currSearchIdx;
     let hasFraction = false;
 
-    const out = arr1.map((obj, idx) => {
+    const result = arr1.map((obj, idx) => {
 
         // TODO - numerator and denominator need to be formatted appropriately into the metaList.
-        const { value: currValue, metaList: currMetaList, numerator: currNumerator, denominator: currDenominator } = obj;
+        const { value: currValue, metaList: currMetaList } = obj;
         const newObj = { metaList: currMetaList || [] };
         const desiredBuddy = desiredProduct / currValue;
         const distanceFromDesired = (n) => Math.abs(desiredBuddy - n);
         hasFraction = hasFraction || (0 < currValue && currValue < 1);
 
-        currentSearchIdx = (currentSearchIdx === undefined) ? arr2.length - 1 : currentSearchIdx;
+        currSearchIdx = (currSearchIdx === undefined) ? arr2.length - 1 : currSearchIdx;
 
-        let buddy = arr2[currentSearchIdx];
+        let buddy = arr2[currSearchIdx];
         let distanceFromBuddy = distanceFromDesired(buddy.value);
 
-        // TODO - validate I am choosing the correct numbers inside this loop - if I am off by 1 the answers will be close, but incorrect!
-        // TODO - validate I am choosing the correct numbers inside this loop - if I am off by 1 the answers will be close, but incorrect!
         findBestBuddy :
-            for (var i = (currentSearchIdx - 1); i >= 0; i--) {
+            for (let i = (currSearchIdx - 1); i >= 0; i--) {
 
                 const currCandidate = arr2[i];
                 const currDelta = distanceFromDesired(currCandidate.value);
@@ -35,6 +34,7 @@ module.exports = (arr1, arr2, desiredProduct) => {
                     distanceFromBuddy = currDelta;
                     buddy = currCandidate;
                 } else {
+                    currSearchIdx = i + 1;
                     break findBestBuddy;
                 }
             }
@@ -54,9 +54,11 @@ module.exports = (arr1, arr2, desiredProduct) => {
         return newObj;
     });
 
-    if (!hasFraction) {
-        console.warn('WARNING! None of the values passed to mapClosestProduct are fractions. This will limit the effectiveness of your solution');
-    }
-    console.log('found closest product ', closestProduct);
-    return out;
+    return {
+        result,
+        meta : {
+            closestProduct,
+            warning : hasFraction ? undefined : noFractionWarning
+        }
+    };
 };
--- test/tests/mapClosestProduct_spec.js
+++ test/tests/mapClosestProduct_spec.js
@@ -4,6 +4,8 @@
 // const timer = require('../helpers/timer');
 const { expect } = require('chai');
 const mapClosestProduct = require('../../src/mapClosestProduct');
+const mergeSort = require('../../src/mergeSort');
+const sortIt = arr => mergeSort(arr, o => o.value);
 
 describe("mapClosestProduct", () => {
     const arr1 = [
@@ -24,26 +26,57 @@ describe("mapClosestProduct", () => {
     let result;
 
     it('should return an array the same length as the first array passed', () => {
-        result = mapClosestProduct(arr1, arr2, 9);
+        result = mapClosestProduct(arr1, arr2, 9).result;
         expect(result.length).to.equal(arr1.length);
     });
 
     it('should be able to take in an array that it already produced and refine the set further', () => {
-        result = mapClosestProduct(result, arr2, 9);
-        result = mapClosestProduct(result, arr2, 9);
+        result = mapClosestProduct(result, arr2, 9).result;
+        result = sortIt(result);
+        result = mapClosestProduct(result, arr2, 9).result;
         expect(result.length).to.equal(arr1.length);
     });
 
+    it('should should calculate the closest result in the meta information', () => {
+        const res = mapClosestProduct(result, arr2, 9);
+        expect(res.meta.closestProduct).to.equal(9);
+    });
+
+    it("should provide a helpful message if you don't include any fractions in either input array", () => {
+        const res = mapClosestProduct(result, arr2, 9);
+        expect(res.meta.warning).to.be.a('string');
+    });
+
     it('should refine the results to get closer to your target if you use some fractions', () => {
-        arr1.push({ value: 3/4, metaList: ['fraction 1']});
-        arr2.push({ value: 5/11, metaList: ['fraction 2']});
-        result = mapClosestProduct(arr1, arr2, 9);
-        result = mapClosestProduct(result, arr2, 9);
-        result = mapClosestProduct(result, arr2, 9);
-        result = mapClosestProduct(result, arr2, 9);
-        result = mapClosestProduct(result, arr2, 9);
+        arr1.unshift({ value: 3/4, metaList: ['fraction 1']});
+        arr2.unshift({ value: 5/11, metaList: ['fraction 2']});
+        result = mapClosestProduct(arr1, arr2, 9).result;
+        result = sortIt(result);
+        result = mapClosestProduct(result, arr2, 9).result;
+        result = sortIt(result);
+        result = mapClosestProduct(result, arr2, 9).result;
+        result = sortIt(result);
+        result = mapClosestProduct(result, arr2, 9).result;
+        result = sortIt(result);
+        result = mapClosestProduct(result, arr2, 9).result;
+        console.log('this one should be closer');
+        console.log('this one should be closer');
         console.log(result);
         expect(result.length).to.equal(arr1.length);
+    });
+
+    it("should have an undefined warning if you do include fractions in your input arrays", () => {
+        const res = mapClosestProduct(result, arr2, 9);
+        expect(res.meta.warning).to.equal(undefined);
+    });
+
+    it("should accurately calculate closest values based on passed inputs", () => {
+        const res = mapClosestProduct(arr1, arr2, 9);
+        expect(res[0].value).to.equal(9);
+        expect(res[0].value).to.equal(9);
+        expect(res[0].value).to.equal(9);
+        expect(res[0].value).to.equal(9);
+        expect(res[0].value).to.equal(9);
     });
 
 });
