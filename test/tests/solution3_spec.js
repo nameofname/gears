ff8b9b09c6593e460bfb913ef38fb2f2dcac8506
nameofname <nameofemail@gmail.com>
2017-05-10 00:07:22 -0400

    updated comment in test

--- test/tests/solution3_spec.js
+++ test/tests/solution3_spec.js
@@ -4,13 +4,14 @@ const timer = require('../helpers/timer');
 const { expect } = require('chai');
 const solution3 = require('../../src/solution3');
 
-describe("solution3", () => {
+describe.only("solution3", () => {
 
     it('should return an object with a result array, and meta information', () => {
         const result = solution3({desiredNumber: 127, requiredFactors: [40], numberOfPairs: 3, lowerBound: 13, upperBound: 50});
         expect(Array.isArray(result.result)).to.equal(true);
         expect(result.meta instanceof Object).to.equal(true);
-        console.log(JSON.stringify(result.meta))
+        // console.log(JSON.stringify(result.meta))
+        console.log(result.meta.closestProduct * 40)
     });
 
     it('should be very, very fast', () => {
